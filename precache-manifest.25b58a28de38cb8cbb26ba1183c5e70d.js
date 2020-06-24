a68509fe02929c55d61d102e81590505ae4084ab
Parth Darji <parthdarji55@gmail.com>
2020-06-24 16:41:35 -0400

    Updates

---
+++ precache-manifest.25b58a28de38cb8cbb26ba1183c5e70d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da30307255b32dd88539aea20b6608a4",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "8c2ddd883ce8ae1a5c6a",
+    "url": "/personal-website/static/css/main.9d982025.chunk.css"
+  },
+  {
+    "revision": "c12b69cc5c5713d4a442",
+    "url": "/personal-website/static/js/2.3c4f1c78.chunk.js"
+  },
+  {
+    "revision": "8c2ddd883ce8ae1a5c6a",
+    "url": "/personal-website/static/js/main.02077f2c.chunk.js"
+  },
+  {
+    "revision": "5b55a1e577641beecab3",
+    "url": "/personal-website/static/js/runtime-main.e1259172.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal-website/precache-manifest.74a4491afcbfbb7ded8e785fccc480c8.js"
+  "/personal-website/precache-manifest.25b58a28de38cb8cbb26ba1183c5e70d.js"
 );
 
 self.addEventListener('message', (event) => {
